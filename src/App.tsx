b28ccccb283f409974c7feed6db080ccb7de615a
Ishita Mengi <mengiishita@gmail.com>
2023-07-27 17:55:29 +0200

    Integrating tailwind cdd and defining all the routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <h1>Notes app</h1>
+    <div className="m-6 text-3xl font-bold">
+      <Routes>
+        <Route path="/" element={<h1>HI</h1>} />
+        <Route path="/new" element={<h1>New</h1>} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </div>
   );
 }
 
